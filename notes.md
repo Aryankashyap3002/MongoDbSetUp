04b7e045720b86818975750b8fb300f03e3a7f5b
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-12-20 01:27:36 +0530

    MongoDb setUp

---
+++ notes.md
@@ -0,0 +1,6 @@
+Schema is generally use to make sure consistent object sharing or consistent model sharing.
+
+Difference between relational schema and nosql schema is here u not require to fill every entries but in relational schema u have to fill null.
+Schema is for blueprint.
+
+findByIdAndUpdate return old values for new values u need to pass one more argument { new: true } inside the function
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const connect = require('./config/database')
+const app = express()
+
+const TweetRepository = require('./repository/tweet-repository')
+const Comment = require('./models/comment')
+
+app.listen(3000, async () => {
+    console.log('server started');
+    await connect();
+    console.log('mongodb is connected')
+    // const tweet = await Tweet.create({
+    //     content: "First tweet",
+    //     userEmail: "a@b.com"
+    // });
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.create({content: 'Tweer with comment schema'})
+    const comment = await Comment.create({content: 'new comment'})
+    tweet.comments.push(comment)
+    await tweet.save()
+    console.log(tweet)
+    
+}) 
